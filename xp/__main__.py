b7129155e8d35f4622c167da6aeddf1340253742
Ben Jeffrey <mail@benjeffrey.net>
2017-10-24 15:47:41 +0100

    Query input using first argument as XPath.

--- xp/__main__.py
+++ xp/__main__.py
@@ -1,12 +1,18 @@
 import sys
-from lxml import etree
 
+from lxml import etree
 from pygments import highlight
-from pygments.formatters.terminal256 import Terminal256Formatter
+from pygments.formatters.terminal import TerminalFormatter
 from pygments.lexers.html import XmlLexer
 
 
+query = sys.argv[1] if len(sys.argv) > 1 else None
 input = etree.parse(sys.stdin)
-output = etree.tostring(input, pretty_print=True)
 
-print(highlight(output, XmlLexer(), Terminal256Formatter()))
+if query:
+    matches = [etree.tostring(el, pretty_print=True) for el in input.xpath(query)]
+    output = b'<results>\n' + b''.join(matches) + b'</results>'
+else:
+    output = etree.tostring(input, pretty_print=True)
+
+print(highlight(output, XmlLexer(), TerminalFormatter()))
