8ce0b3c6748705d39262d8d924865fe827347e4e
Ben Jeffrey <mail@benjeffrey.net>
2017-10-24 22:27:53 +0100

    Return results as an XML datastructure.

--- xp/__main__.py
+++ xp/__main__.py
@@ -2,25 +2,30 @@ import sys
 from typing import Union
 
 from lxml import etree
+from lxml.builder import E
 from pygments import highlight
 from pygments.formatters.terminal import TerminalFormatter
 from pygments.lexers.html import XmlLexer
 
 
-def to_str(x: Union[etree.Element, str]) -> str:
-    try:
-        return etree.tounicode(x, pretty_print=True).strip()
-    except TypeError:
-        return x
+def wrap_in_results(elements: [Union[etree.Element, etree._ElementUnicodeResult]]) -> str:
+    results = E.results()
+    for el in elements:
+        results.append(E.result(el))
+    return results
 
+
+# See <http://lxml.de/FAQ.html#why-doesn-t-the-pretty-print-option-reformat-my-xml-output>
+parser = etree.XMLParser(remove_blank_text=True)
+input = etree.parse(sys.stdin, parser)
 
 query = sys.argv[1] if len(sys.argv) > 1 else None
-input = etree.parse(sys.stdin)
-
 if query:
     matches = input.xpath(query)
-    output = '\n'.join([to_str(el) for el in matches])
+    results = wrap_in_results(matches)
+    output = etree.tostring(results, pretty_print=True)
 else:
     output = etree.tostring(input, pretty_print=True)
 
-print(highlight(output, XmlLexer(), TerminalFormatter()).strip())
+
+sys.stdout.write(highlight(output, XmlLexer(), TerminalFormatter()))
