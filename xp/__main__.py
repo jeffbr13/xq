2d2766dc4cd2191dbae57ddc9e41b0170ea0c125
Ben Jeffrey <mail@benjeffrey.net>
2017-10-24 19:55:34 +0100

    Allow returning bare text and strip whitespace between results.

--- xp/__main__.py
+++ xp/__main__.py
@@ -1,4 +1,5 @@
 import sys
+from typing import Union
 
 from lxml import etree
 from pygments import highlight
@@ -6,13 +7,20 @@ from pygments.formatters.terminal import TerminalFormatter
 from pygments.lexers.html import XmlLexer
 
 
+def to_str(x: Union[etree.Element, str]) -> str:
+    try:
+        return etree.tounicode(x, pretty_print=True).strip()
+    except TypeError:
+        return x
+
+
 query = sys.argv[1] if len(sys.argv) > 1 else None
 input = etree.parse(sys.stdin)
 
 if query:
-    matches = [etree.tostring(el, pretty_print=True) for el in input.xpath(query)]
-    output = b'<results>\n' + b''.join(matches) + b'</results>'
+    matches = input.xpath(query)
+    output = '\n'.join([to_str(el) for el in matches])
 else:
     output = etree.tostring(input, pretty_print=True)
 
-print(highlight(output, XmlLexer(), TerminalFormatter()))
+print(highlight(output, XmlLexer(), TerminalFormatter()).strip())
